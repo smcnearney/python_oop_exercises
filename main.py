28cf73bab6ebbf9dd03a4eedbf93d362cdd783fa
Stephen McNearney <stephen@hownow.media>
2020-12-15 14:49:52 -0500

    First commit to branch Python_OOP_Exercises-2 - exercises up until bonus

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-# from person import Person
+from person import Person
 
-# sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
-# jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
+sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
+jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
 
-# sonny.greet(jordan)
-# jordan.greet(sonny)
+sonny.greet(jordan)
+jordan.greet(sonny)
 
 # print(sonny.contact_info())
 # print(jordan.contact_info())
 
-from vehicles import Vehicle
+sonny.print_contact_info()
 
-car = Vehicle('BMW','2002tii',1969)
+sonny.print_contact_info() 
 
-print(car.print_info())
-print(car)
+jordan.add_friend(sonny)
+
+jordan.num_friends()
+
+jordan.greeting_count()
+
+print(jordan)
+
+# from vehicles import Vehicle
+
+# car = Vehicle('BMW','2002tii',1969)
+
+# print(car.print_info())
+# print(car)
--- person.py
+++ person.py
@@ -1,11 +1,39 @@
 class Person:
-    def __init__(self, name, email, phone):
+     def __init__(self, name, email, phone):
          self.name = name
          self.email = email
          self.phone = phone
+         self.friends = []
+         self.count_greetings = 0
 
-    def greet(self, other_person):
+     def greet(self, other_person):
          print('Hello {}, I am {}!'.format(other_person.name, self.name))
+         self.count_greetings += 1
 
-    def contact_info(self):
+     def contact_info(self):
         return "{}\'s e-mail address: {} and phone number: {}".format(self.name, self.email, self.phone)
+    
+     def print_contact_info(self):
+         return "{}\'s e-mail address: {} and phone number: {}".format(self.name, self.email, self.phone)
+
+     def add_friend(self, new_friend):
+          self.friends.append(new_friend)
+     
+     def num_friends(self):
+          print(len(self.friends))
+     
+     def greeting_count(self):
+          print(self.count_greetings)
+     
+     def __str__(self):
+          return """
+          %s
+          %s
+          %s
+          %s
+          """ % (self.name, self.email, self.phone, self.count_greetings)
+     
+
+          
+
+     
